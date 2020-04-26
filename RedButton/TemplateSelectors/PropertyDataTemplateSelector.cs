e0fabec5efd9b770eb3f2022014ddf80b22a6818
Chunlin <chunlin@nus.edu.sg>
2020-04-26 12:59:19 +0800

    Added ViewModels, TemplateSelectors, and Helpers.

---
+++ RedButton/TemplateSelectors/PropertyDataTemplateSelector.cs
@@ -0,0 +1,58 @@
+﻿using RedButton.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace RedButton.TemplateSelectors
+{
+    public class PropertyDataTemplateSelector : DataTemplateSelector
+    {
+        /// <summary>
+        /// Gets or sets the text property.
+        /// </summary>
+        public DataTemplate TextPropertyTemplate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the boolean property.
+        /// </summary>
+        public DataTemplate BooleanPropertyTemplate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the int property.
+        /// </summary>
+        public DataTemplate NumericPropertyTemplate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the color property.
+        /// </summary>
+        public DataTemplate ColorPropertyTemplate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the blank.
+        /// </summary>
+        public DataTemplate BlankTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, Windows.UI.Xaml.DependencyObject container)
+        {
+            if (item is null) return BlankTemplate;
+
+            var customObject = (item as PropertyObjectViewModel);
+            var type = customObject.PropertyValue;
+
+            if (type is null) return BlankTemplate;
+            if (type is string) return TextPropertyTemplate;
+            if (type is bool) return BooleanPropertyTemplate;
+            if (type is int) return NumericPropertyTemplate;
+            if (type is double) return NumericPropertyTemplate;
+            if (type is float) return NumericPropertyTemplate;
+            if (type is Color) return ColorPropertyTemplate;
+
+            return BlankTemplate;
+        }
+    }
+}
---
+++ RedButton/ViewModels/PropertyObjectViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedButton.ViewModels
+{
+    public class PropertyObjectViewModel : INotifyPropertyChanged
+    {
+        private object _propertyValue;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyObjectViewModel"/> class.
+        /// </summary>
+        /// <param name="headerGroupName">The name.</param>
+        public PropertyObjectViewModel(string headerGroupName)
+        {
+            HeaderGroupName = headerGroupName;
+        }
+
+        /// <summary>
+        /// Gets or sets the header group.
+        /// </summary>
+        public string HeaderGroupName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the property.
+        /// </summary>
+        public string PropertyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the property description.
+        /// </summary>
+        public string PropertyDescription { get; set; }
+
+        /// <summary>
+        /// Gets or sets the property value.
+        /// </summary>
+        public object PropertyValue
+        {
+            get => _propertyValue;
+            set
+            {
+                _propertyValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
