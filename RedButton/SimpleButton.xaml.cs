606e7d035dcb7d72ca634e54d04945235ea967e5
Chunlin <chunlin@nus.edu.sg>
2020-04-26 13:10:08 +0800

    Implemented some Telerik controls.

--- RedButton/SimpleButton.xaml.cs
+++ RedButton/SimpleButton.xaml.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using RedButton.Helpers;
+using RedButton.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Telerik.Data.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -19,9 +24,91 @@ namespace RedButton
 {
     public sealed partial class SimpleButton : UserControl
     {
+        public PropertyCollection Properties
+        {
+            get { return (PropertyCollection)GetValue(PropertiesProperty); }
+            set { SetValue(PropertiesProperty, value); }
+        }
+
+        public static readonly DependencyProperty PropertiesProperty =
+            DependencyProperty.Register("Properties",
+                typeof(PropertyCollection),
+                typeof(SimpleButton),
+                new PropertyMetadata(null, (d, e) =>
+                {
+                    var ctrl = d as SimpleButton;
+                    if (e.NewValue is null) return;
+
+                    var data = e.NewValue as PropertyCollection;
+
+                    var list = new ObservableCollection<PropertyObjectViewModel>();
+                    foreach (PropertyObjectViewModel item in data.Values)
+                    {
+                        list.Add(item);
+                    }
+                    ctrl.propertyGrid.ItemsSource = list.OrderBy(p => p.PropertyName);
+                }));
+
         public SimpleButton()
         {
             this.InitializeComponent();
         }
+
+        public event RoutedEventHandler ValuesChanged;
+
+        private void ArrangedBySelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (propertyGrid == null) return;
+
+            var comboBox = sender as ComboBox;
+            var selectedItem = ((ComboBoxItem)comboBox.SelectedItem).Content as string;
+
+            propertyGrid.GroupDescriptors.Clear();
+
+            if (selectedItem == "Category")
+            {
+                var descriptor = new PropertyGroupDescriptor();
+                descriptor.PropertyName = "HeaderGroupName";
+                propertyGrid.GroupDescriptors.Add(descriptor);
+            }
+
+        }
+
+        private void SearchClicked(object sender, RoutedEventArgs e)
+        {
+            FindProperty();
+        }
+
+        private void SearchBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            FindProperty();
+        }
+
+
+        private void FindProperty()
+        {
+            var descriptor = new TextFilterDescriptor();
+            propertyGrid.FilterDescriptors.Clear();
+            descriptor.PropertyName = "PropertyName";
+            descriptor.IsCaseSensitive = false;
+            descriptor.Value = SearchBox.Text;
+            descriptor.Operator = TextOperator.Contains;
+            propertyGrid.FilterDescriptors.Add(descriptor);
+            if (SearchBox.Text.Length > 0)
+            {
+                Search.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                Search.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValuesChanged?.Invoke(this, new PropertyWindowValuesChangedArgs());
+        }
     }
+
+
 }
